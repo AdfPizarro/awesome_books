27ede27ee018d68691ba65c3215e55b529512b22
Adrian Flores Pizarro <adfpizarro@gmail.com>
2021-08-09 15:59:46 -0600

    Fix linter issues

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ if (localStorage.getItem('bookList') === null) {
   bookList = JSON.parse(localStorage.getItem('bookList'));
 }
 
+function removeBook(index) {
+  bookList = bookList.filter((_, i) => i !== index);
+  localStorage.setItem('bookList', JSON.stringify(bookList));
+  refresh();// eslint-disable-line no-use-before-define
+}
+
 function refresh() {
   const getBookList = document.getElementById('bookList');
   getBookList.innerHTML = '';
@@ -12,15 +18,25 @@ function refresh() {
     const div = document.createElement('div');
     const title = document.createElement('h3');
     const author = document.createElement('h3');
+    const button = document.createElement('button');
 
     title.textContent = bookList[i].title;
     author.textContent = bookList[i].author;
+    button.textContent = 'Delete';
+    button.setAttribute('id', i);
+    button.setAttribute('class', 'delete');
 
     div.append(title);
     div.append(author);
+    div.append(button);
 
     getBookList.appendChild(div);
   }
+
+  const dltButtons = document.querySelectorAll('.delete');
+  dltButtons.forEach((el) => el.addEventListener('click', (event) => { // eslint-disable-line no-unused-vars
+    removeBook(parseInt(el.id, 10));
+  }));
 }
 
 window.onload = function () {
@@ -43,11 +59,3 @@ function addBook() {
 }
 
 document.getElementById('addBook').addEventListener('click', addBook);
-
-/* eslint-disable no-unused-vars */
-function removeBook(index) {
-  bookList = bookList.filter((_, i) => i !== index);
-  localStorage.setItem('bookList', JSON.stringify(bookList));
-  refresh();
-}
-/* eslint-enable no-unused-vars */
