e0ce0f99dade06b08f9b188310268b4c3a8e6d85
mina <takhminam@gmail.com>
2021-08-09 23:48:42 +0300

    Add refresh function

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 var bookList= [];
 
-const Book = (title, author)=> ({
-  title, author
-});
+class Book{
+  constructor(title, author){
+  this.title = title;
+  this.author = author;
+  
+}
+}
 
-document.getElementById("addBook").addEventListener("click", function(){
-  var author=document.getElementById("bookAuthor").value;
-  var title=document.getElementById("bookTitle").value;
+document.getElementById("addBook").addEventListener("click", add_book);
 
-  bookList.push(Book(title, author));
-  console.log(bookList)
-  draw()
-});
+function add_book(){
+var title = document.getElementById("bookTitle").value
+var author = document.getElementById("bookAuthor").value
+bookList.push(new Book(title, author))
+console.log(bookList)
+refresh()
+}
 
-function remove(bookId) {
-  bookList.splice(bookId, 1);
-  console.log(bookList);
+
+function remove_book(arr, index) {
+    console.log(arr.filter((_, i) => i !== index)) 
+    refresh()
+}
+
+function refresh(){
+  
+   var get_bookList = document.getElementById("bookList")
+   get_bookList.innerHTML = ''
+   for(let i = 0; i < bookList.length; i ++){ 
+   var div = document.createElement("div");
+   var title = document.createElement('h3');
+   var author = document.createElement('h3');
+
+   title.textContent = bookList[i].title ;
+   author.textContent = bookList[i].author;
+
+   div.append(title); 
+   div.append(author); 
+   
+   get_bookList.appendChild(div) 
+
+   }
+
 }
