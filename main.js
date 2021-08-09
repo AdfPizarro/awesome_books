0a4c6b36a6b404b148d380215ccefb6547f6fcb9
Adrian Flores Pizarro <adfpizarro@gmail.com>
2021-08-09 15:33:29 -0600

    Fix linter issues

--- main.js
+++ main.js
@@ -1,61 +1,53 @@
-var bookList= [];
+let bookList = [];
 if (localStorage.getItem('bookList') === null) {
   bookList = [];
 } else {
   bookList = JSON.parse(localStorage.getItem('bookList'));
 }
 
+function refresh() {
+  const getBookList = document.getElementById('bookList');
+  getBookList.innerHTML = '';
+  for (let i = 0; i < bookList.length; i += 1) {
+    const div = document.createElement('div');
+    const title = document.createElement('h3');
+    const author = document.createElement('h3');
 
+    title.textContent = bookList[i].title;
+    author.textContent = bookList[i].author;
 
-window.onload = function() {
-  refresh();
-};
+    div.append(title);
+    div.append(author);
 
-
-
-class Book{
-  constructor(title, author){
-  this.title = title;
-  this.author = author;
+    getBookList.appendChild(div);
   }
 }
 
-function addBook(){
-    let title = document.getElementById("bookTitle").value
-    let author = document.getElementById("bookAuthor").value
-    bookList.push(new Book(title, author))
-    localStorage.setItem('bookList', JSON.stringify(bookList));
-    refresh()
+window.onload = function () {
+  refresh();
+};
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 
-
-document.getElementById("addBook").addEventListener("click", addBook);
-
-
-
-function removeBook(arr, index) {
-    console.log(arr.filter((_, i) => i !== index))
-    localStorage.setItem('bookList', JSON.stringify(bookList));
-    refresh()
+function addBook() {
+  const title = document.getElementById('bookTitle').value;
+  const author = document.getElementById('bookAuthor').value;
+  bookList.push(new Book(title, author));
+  localStorage.setItem('bookList', JSON.stringify(bookList));
+  refresh();
 }
 
-function refresh(){
+document.getElementById('addBook').addEventListener('click', addBook);
 
-   var get_bookList = document.getElementById("bookList")
-   get_bookList.innerHTML = ''
-   for(let i = 0; i < bookList.length; i ++){
-   var div = document.createElement("div");
-   var title = document.createElement('h3');
-   var author = document.createElement('h3');
-
-   title.textContent = bookList[i].title ;
-   author.textContent = bookList[i].author;
-
-   div.append(title);
-   div.append(author);
-
-   get_bookList.appendChild(div)
-
-   }
-
+/* eslint-disable no-unused-vars */
+function removeBook(index) {
+  bookList = bookList.filter((_, i) => i !== index);
+  localStorage.setItem('bookList', JSON.stringify(bookList));
+  refresh();
 }
+/* eslint-enable no-unused-vars */
