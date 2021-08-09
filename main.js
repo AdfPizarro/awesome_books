90b3a2c31ab69279d4a28541dd7de5cff5bcbcdd
Adrian Flores Pizarro <adfpizarro@gmail.com>
2021-08-09 15:09:39 -0600

    Add localstorage

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
 var bookList= [];
+if (localStorage.getItem('bookList') === null) {
+  bookList = [];
+} else {
+  bookList = JSON.parse(localStorage.getItem('bookList'));
+}
+
+
+
+window.onload = function() {
+  refresh();
+};
+
+
 
 class Book{
   constructor(title, author){
   this.title = title;
   this.author = author;
-  
-}
+  }
 }
 
-document.getElementById("addBook").addEventListener("click", add_book);
-
-function add_book(){
-var title = document.getElementById("bookTitle").value
-var author = document.getElementById("bookAuthor").value
-bookList.push(new Book(title, author))
-console.log(bookList)
-refresh()
+function addBook(){
+    let title = document.getElementById("bookTitle").value
+    let author = document.getElementById("bookAuthor").value
+    bookList.push(new Book(title, author))
+    localStorage.setItem('bookList', JSON.stringify(bookList));
+    refresh()
 }
 
 
-function remove_book(arr, index) {
-    console.log(arr.filter((_, i) => i !== index)) 
+document.getElementById("addBook").addEventListener("click", addBook);
+
+
+
+function removeBook(arr, index) {
+    console.log(arr.filter((_, i) => i !== index))
+    localStorage.setItem('bookList', JSON.stringify(bookList));
     refresh()
 }
 
 function refresh(){
-  
+
    var get_bookList = document.getElementById("bookList")
    get_bookList.innerHTML = ''
-   for(let i = 0; i < bookList.length; i ++){ 
+   for(let i = 0; i < bookList.length; i ++){
    var div = document.createElement("div");
    var title = document.createElement('h3');
    var author = document.createElement('h3');
@@ -36,10 +51,10 @@ function refresh(){
    title.textContent = bookList[i].title ;
    author.textContent = bookList[i].author;
 
-   div.append(title); 
-   div.append(author); 
-   
-   get_bookList.appendChild(div) 
+   div.append(title);
+   div.append(author);
+
+   get_bookList.appendChild(div)
 
    }
 
