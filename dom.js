77fbbf02ecf1faaf1d74abc8dd477f6bf46f815a
Adrian Flores Pizarro <adfpizarro@gmail.com>
2021-08-11 11:58:56 -0600

    Add styling to the booklist

--- dom.js
+++ dom.js
@@ -7,18 +7,27 @@ class Dom { // eslint-disable-line no-unused-vars
     this.getBookList.innerHTML = '';
     for (let i = 0; i < bookList.length; i += 1) {
       const div = document.createElement('div');
+      const bookInfo = document.createElement('div');
       const title = document.createElement('h3');
       const author = document.createElement('h3');
       const button = document.createElement('button');
+      const span = document.createElement('span');
 
       title.textContent = bookList[i].title;
       author.textContent = bookList[i].author;
+      span.textContent = ' by ';
+      bookInfo.setAttribute('class', 'bookInfo');
       button.textContent = 'Delete';
       button.setAttribute('id', i);
+      div.setAttribute('class', 'book');
       button.setAttribute('class', 'delete');
 
-      div.append(title);
-      div.append(author);
+      bookInfo.append(title);
+      bookInfo.append(span);
+      bookInfo.append(author);
+
+      div.append(bookInfo);
+
       div.append(button);
 
       this.getBookList.appendChild(div);
