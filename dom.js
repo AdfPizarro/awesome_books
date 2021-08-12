f17142008efa408686201042557c703f8e645c73
mina <takhminam@gmail.com>
2021-08-12 17:48:01 +0300

    Fix styling issue

--- dom.js
+++ dom.js
@@ -1,25 +1,34 @@
-class Dom { // eslint-disable-line no-unused-vars
+class Dom {
+  // eslint-disable-line no-unused-vars
   constructor() {
-    this.getBookList = document.getElementById('bookList');
+    this.getBookList = document.getElementById("bookList");
   }
 
-
   refresh(bookList) {
-    this.getBookList.innerHTML = '';
+    this.getBookList.innerHTML = "";
     for (let i = 0; i < bookList.length; i += 1) {
-      const div = document.createElement('div');
-      const title = document.createElement('h3');
-      const author = document.createElement('h3');
-      const button = document.createElement('button');
+      const div = document.createElement("div");
+      const bookInfo = document.createElement("div");
+      const title = document.createElement("h3");
+      const author = document.createElement("h3");
+      const button = document.createElement("button");
+      const span = document.createElement("span");
 
       title.textContent = bookList[i].title;
       author.textContent = bookList[i].author;
-      button.textContent = 'Delete';
-      button.setAttribute('id', i);
-      button.setAttribute('class', 'delete');
+      span.textContent = " by ";
+      bookInfo.setAttribute("class", "bookInfo");
+      button.textContent = "Delete";
+      button.setAttribute("id", i);
+      div.setAttribute("class", "book");
+      button.setAttribute("class", "delete");
 
-      div.append(title);
-      div.append(author);
+      bookInfo.append(title);
+      bookInfo.append(span);
+      bookInfo.append(author);
+
+      div.append(bookInfo);
+
       div.append(button);
 
       this.getBookList.appendChild(div);
